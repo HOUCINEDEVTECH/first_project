05aafbe7ee7d4048b87261260549d8cd40326432
ounis houcine <ounishoucine.on@gmail.com>
2023-01-31 18:30:11 +0100

    delete file cheking_number.py

--- Checking_number.py
+++
@@ -1,8 +0,0 @@
-# task   checking number
-number =int(input(" isert you number : " ))
-if number  <  0 :
-     print(" the number is negative  ")
-elif number  > 0  :
-     print(' the number is  positive  ')
-else :
-    print(' th number is zero')
